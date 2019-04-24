49339f86451cde7acf6c871df366a9fd34de9a1f
Chenbo Yi <yichenbo@outlook.com>
2019-04-24 11:53:16 +0100

    connect with the database

--- lib/bookmarks.rb
+++ lib/bookmarks.rb
@@ -1,6 +1,22 @@
+require 'pg'
+
 class Bookmarks
 
+  def initialize(data = nil)
+    @data = data
+  end
+
   def all
-    ['www.google.com', 'www.youtube.com', 'www.instagram.com']
+    retrive_data
+    @data.map { |row| row["url"] }
+  end
+
+  private
+
+  def retrive_data
+    connection = PG.connect(dbname: 'bookmark_manager')
+    @data = connection.exec("SELECT * FROM bookmarks") do |result|
+      result.map { |row| row }
+    end
   end
 end
